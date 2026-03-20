6b495455bf7916fb83196dfb88b02d753d298bd2
Baphomet42 <71295013+Baphomet42@users.noreply.github.com>
2026-03-19 22:51:41 -0500

    .

--- src/main/java/baphomethlabs/fortytwoedit/mixin/LocatorBarRendererMixin.java
+++ src/main/java/baphomethlabs/fortytwoedit/mixin/LocatorBarRendererMixin.java
@@ -30,7 +30,7 @@ public abstract class LocatorBarRendererMixin {
     private static boolean isUpsideDown = false;
     private static int size = 0;
 
-    @Inject(method = "extractRenderState", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphicsExtractor;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIIII)V"), cancellable = true)
+    @Inject(method = "lambda$extractRenderState$1", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphicsExtractor;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIIII)V"), cancellable = true)
     public void injectExtractRenderState(Entity entity, Level level, PartialTickSupplier partialTickSupplier, GuiGraphicsExtractor guiGraphics, int i, TrackedWaypoint trackedWaypoint, CallbackInfo ci) {
 
         playerInfo = null;
@@ -39,28 +39,30 @@ public abstract class LocatorBarRendererMixin {
         && trackedWaypoint.id().left().isPresent()) {
             final Minecraft minecraft = Minecraft.getInstance();
             playerInfo = minecraft.player.connection.getPlayerInfo(trackedWaypoint.id().left().get());
-            Player player = minecraft.level.getPlayerByUUID(playerInfo.getProfile().id());
-            isUpsideDown = player != null && AvatarRenderer.isPlayerUpsideDown(player);
+            if (playerInfo != null) {
+                Player player = minecraft.level.getPlayerByUUID(playerInfo.getProfile().id());
+                isUpsideDown = player != null && AvatarRenderer.isPlayerUpsideDown(player);
 
-            WaypointStyle waypointStyle = minecraft.getWaypointStyles().get(trackedWaypoint.icon().style);
-            float f = Mth.sqrt((float)trackedWaypoint.distanceSquared(entity));
-            size = 0;
-            if (f < waypointStyle.nearDistance())
-                size = 1;
-            else if (f >= waypointStyle.farDistance())
-                size = -1;
+                WaypointStyle waypointStyle = minecraft.getWaypointStyles().get(trackedWaypoint.icon().style);
+                float f = Mth.sqrt((float)trackedWaypoint.distanceSquared(entity));
+                size = 0;
+                if (f < waypointStyle.nearDistance())
+                    size = 1;
+                else if (f >= waypointStyle.farDistance())
+                    size = -1;
+            }
         }
 
     }
 
-	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphicsExtractor;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIIII)V"))
-	private void redirectBlitSprite(GuiGraphicsExtractor guiGraphics, RenderPipeline renderPipeline, Identifier identifier, int i, int j, int k, int l, int m) {
+	@Redirect(method = "lambda$extractRenderState$1", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphicsExtractor;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIIII)V"))
+	private void redirectBlitSprite(GuiGraphicsExtractor guiGraphics, final RenderPipeline renderPipeline, final Identifier location, final int x, final int y, final int width, final int height, final int color) {
         if (playerInfo == null)
-            guiGraphics.blitSprite(renderPipeline, identifier, i, j, k, l, m);
+            guiGraphics.blitSprite(renderPipeline, location, x, y, width, height, color);
         else {
             if (FortytwoEdit.mixinLocatorBarColor)
-                guiGraphics.fill(i-size, j-size, i+k+size, j+k+size, m);
-            PlayerFaceExtractor.extractRenderState(guiGraphics, playerInfo.getSkin().body().texturePath(), i+1-size, j+1-size, k-2+size+size, playerInfo.showHat(), isUpsideDown, -1);
+                guiGraphics.fill(x-size, y-size, x+width+size, y+height+size, color);
+            PlayerFaceExtractor.extractRenderState(guiGraphics, playerInfo.getSkin().body().texturePath(), x+1-size, y+1-size, width-2+size+size, playerInfo.showHat(), isUpsideDown, -1);
         }
 	}
     
